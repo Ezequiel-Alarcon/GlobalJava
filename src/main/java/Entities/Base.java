fa2a1a74e81d50c98aab85d09dd4693d6fd6f710
Ezequiel <ezequielw22l@gmail.com>
2025-06-09 14:12:47 -0300

    se agrego ToString, Equals y HashCode

--- src/main/java/Entities/Base.java
+++ src/main/java/Entities/Base.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Objects;
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,4 +15,23 @@ import lombok.experimental.SuperBuilder;
 
 public class Base {
     private int id;
+
+    //(?)
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Base base)) return false;
+        return id == base.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Base{" +
+                "id=" + id +
+                '}';
+    }
 }
